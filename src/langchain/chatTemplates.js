506f66cbe621cd9cc27b4a8c139699daac84dc18
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-10-27 13:37:31 +0100

    add templates

---
+++ src/langchain/chatTemplates.js
@@ -0,0 +1,24 @@
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+  PromptTemplate,
+} from "@langchain/core/prompts";
+
+const standaloneTemplate =
+  "Given a question, convert it to a standalone question in Swedish. question: {userInput} standalone question:";
+
+export const standalonePrompt = PromptTemplate.fromTemplate(standaloneTemplate);
+
+export const answerChatPrompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    `You are a friendly customer service bot for TechNova AB. You only answer questions that has an answer in provided context. You also quote the relevant passage from provided context. If the question is not related to TechNova or customer service, you politly tell the user that you are unequipped to answer that type of question. If you don't know the answer to a question, you truthfully say so and advise the user to email customer support at support@technova.se. You always answer int the same language that user uses in the question.
+    context: {context}
+    history: {history}
+    question: {userInput}
+    answer:
+    `,
+  ],
+  new MessagesPlaceholder("history"),
+  ["user", `{userInput}`],
+]);
